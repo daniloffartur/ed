2a66e42a003848ac39ea54f321c9db581c5171bd
Artur <daniloffartur@mail.ru>
2021-12-06 18:23:29 +0300

    l5

--- src/lesson5/Employee.java
+++ src/lesson5/Employee.java
@@ -17,9 +17,6 @@ public class Employee {
         this.age = age;
     }
 
-    public Employee(String jonny, String programmer, String email, String phone) {
-    }
-
     public String info(){
         return String.format("fullName: %1$s, position: %2$s, email: %3$s, phone: %4$s, salary: %5$(,.2f, age: %6$2d",
                 this.fullName, this.position, this.email, this.phone, this.salary, this.age);
